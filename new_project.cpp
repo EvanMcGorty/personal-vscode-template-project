3c13b7a166cb61f7b0268d0e04ae4d502e8cddb4
Evan McGorty <evanjamesmcgorty@gmail.com>
2018-07-20 13:29:38 -0400

    working!

--- new_project.cpp
+++ new_project.cpp
@@ -1,6 +1,23 @@
-#include<filesystem>
+#include<experimental/filesystem>
+#include<iostream>
+
+namespace fs = std::experimental::filesystem;
 
 int main()
 {
-    
+    std::string path_name{"clang++ vscode project template/"};
+
+    std::string new_dir_name;
+
+    std::cin >> new_dir_name;
+
+    fs::path source_path{path_name};
+
+    fs::path dest_path{new_dir_name};
+
+    fs::copy(source_path,dest_path,fs::copy_options::recursive);
+
+    char a;
+    std::cin >> a;
+
 }

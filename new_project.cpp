4889497f4edc4a80b44529a67d239dfa7992e942
Evan McGorty <evanjamesmcgorty@gmail.com>
2018-12-26 22:29:22 -0500

    change expression evaluator include, allow set paths to be chosen at compile time

--- new_project.cpp
+++ new_project.cpp
@@ -3,12 +3,16 @@
 
 namespace fs = std::experimental::filesystem;
 
+//change when you compile if you want set directories
+std::string set_files_origin{""};
+std::string set_files_destination{""};
+
 int main()
 {
     std::string possible_errors = "no errors";
     int ret = 0;
     
-    std::string source_path_name{"project templates/clang++ vscode project template"};
+    std::string source_path_name{set_files_origin + "project templates/clang++ vscode project template"};
 
     std::string dest_path_name;
 
@@ -17,6 +21,12 @@ int main()
         std::cout << "target directory:\n" << std::flush;
 
         std::getline(std::cin,dest_path_name);
+
+        //if the name does not declare a drive (e.g. c: or d:) then assume the folder will be in work.
+        if(dest_path_name.size() < 2 || dest_path_name[1] != ':')
+        {
+            dest_path_name = set_files_destination + dest_path_name;
+        }
 
         fs::path source_path{source_path_name};
 
